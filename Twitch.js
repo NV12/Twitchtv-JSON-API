043ee9687cb0a8fe32f6e6d40e268a10eefae1ac
NV12 <naeimvhora@gmail.com>
2018-01-11 17:05:05 +0530

    implemented better UI

--- Twitch.js
+++ Twitch.js
@@ -1,11 +1,12 @@
 
-var channels=["ESL_SC2","freecodecamp","test_channel"];
+var channels=["ESL_SC2","freecodecamp","test_channel","OgamingSC2"];
 var channelImages=[
 					"https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_image-d6db9488cec97125-300x300.jpeg",
 					"https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png",
-					"https://static-cdn.jtvnw.net/user-default-pictures/27103734-3cda-44d6-a384-f2ab71e4bb85-profile_image-300x300.jpg"
+					"https://static-cdn.jtvnw.net/user-default-pictures/27103734-3cda-44d6-a384-f2ab71e4bb85-profile_image-300x300.jpg",
+					"https://static-cdn.jtvnw.net/jtv_user_pictures/ogamingsc2-profile_image-9021dccf9399929e-70x70.jpeg"
 					];
-var channelName,streamStatus,channelGame,channelImageURL,tagStyle,channelLink;
+var channelName,streamStatus,channelGame,channelImage,tagStyle,channelLink;
 // console.log(channels[0]);
 for(i=0;i<channels.length;i++){
 	
@@ -27,7 +28,7 @@ function makeRequest(i){
 			// console.log(channels[i]);
 			channelName= channels[i];
 			channelLink= "https://www.twitch.tv/"+ channelName;
-			channelImageURL= "<img id='channel-icon' src="+ channelImages[i] +">";
+			channelImage= "<img id='channel-icon' src="+ channelImages[i] +">";
 			// console.log(channelImageURL);
 			tagStyle= "style='display:inline'";
 
@@ -35,15 +36,16 @@ function makeRequest(i){
 			if(data.stream !== null){
 				console.log(data.stream.channel.status);
 				
-				streamStatus= data.stream.channel.status;
+				streamStatus= (data.stream.channel.status).substring(0,15);
 				channelGame= data.stream.game;
 				
-				$("#demo").append( channelImageURL +"<h3 "+ tagStyle +" ><a  class='white-text' target='_blank' href="+ channelLink +">"+ channelName +"</a></h3>"+ "<p "+ tagStyle +">  <i> "+ channelGame + "  "+ streamStatus +"</i></p><br />");
+				$("#demo").append("<div class='row'><div class='col-lg-4 col-md-4 col-sm-4'>"+ channelImage +"</div><div class='col-lg-4 col-md-4 col-sm-4' ><h3 "+ tagStyle +" ><a  class='white-text' target='_blank' href="+ channelLink +">"+ channelName +"</a></h3></div><div class='col-lg-4 col-md-4 col-sm-4'><i> "+ channelGame + "  "+ streamStatus +"</i></div></div>");
+				// $("#demo").append("<div class='row'><div class='col-lg-3'>"+ channelImage +"</div><div class='col-lg-3><h3 "+ tagStyle +" ><a  class='white-text' target='_blank' href="+ channelLink +">"+ channelName +"</a></h3></div>"+ "<div class='col-lg-3' ><i>Hi there </i></div></div>");
 
-			}
+15	}
 			// console.log(channelName);
 			else
-				$("#demo").append( channelImageURL +"<h3 "+ tagStyle +" ><a  class='white-text' target='_blank' href="+ channelLink +">"+ channelName +"</a></h3>"+ "<p "+ tagStyle +">"+ "  offline" +"</p><br />");
+				$("#demo").append("<div class='row'><div class='col-lg-4 col-md-4 col-sm-4'>"+ channelImage +"</div><div class='col-lg-4 col-md-4 col-sm-4' ><h3 "+ tagStyle +" ><a  class='white-text' target='_blank' href="+ channelLink +">"+ channelName +"</a></h3></div><div class='col-lg-4 col-md-4 col-sm-4'><i>offline</i></div>");
 		},
 		error: function(xhr){
 	      alert("Error: "+ xhr.status+ " " + xhr.statusText);
